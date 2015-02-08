a400f24e1ed62b60fdba7789764e8374b6f1f45f
petitpingouin <ph.lespi@gmail.com>
2015-02-08 23:17:39 +0100

    Préparation du projet

---
+++ src/LocIHM/LocationBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LocIHM\LocationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LocIHMLocationBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/LocIHM/LocationBundle/LocIHMLocationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LocIHM\LocationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LocIHMLocationBundle extends Bundle
+{
+}

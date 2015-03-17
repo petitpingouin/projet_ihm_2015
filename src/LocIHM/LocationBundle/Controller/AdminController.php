36d8142d27c0ccd592a0e1a380f38922d19530e2
petitpingouin <ph.lespi@gmail.com>
2015-03-17 01:19:19 +0100

    Dernière ligne droite !

--- src/LocIHM/LocationBundle/Controller/AdminController.php
+++ src/LocIHM/LocationBundle/Controller/AdminController.php
@@ -4,6 +4,13 @@ namespace LocIHM\LocationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+use FOS\UserBundle\Model\UserInterface;
+
+
 
 class AdminController extends Controller
 {
@@ -15,7 +22,7 @@ class AdminController extends Controller
 
     public function usersAction()
     {
-    	$userManager = $container->get('fos_user.user_manager');
+    	$userManager = $this->container->get('fos_user.user_manager');
     	$users = $userManager->findUsers();
 
     	return $this->render('LocIHMLocationBundle:Admin:users.html.twig', array(
@@ -23,8 +30,69 @@ class AdminController extends Controller
     	));
     }
 
-    public function addUserAction()
+
+    /*
+     * $username => username
+     */
+    public function deleteUserAction($username, Request $request)
     {
-    	
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->getUser();
+    	$userManager = $this->get('fos_user.user_manager');
+    	$userdelete = $userManager->findUserByUsername($username);
+
+    	if (!is_object($userdelete) || !$userdelete instanceof UserInterface) {
+            $request->getSession()->getFlashBag()->add('alert', 'Ca c\'est bizarre...');
+        } else {
+	    	if($userdelete === $user) {
+	    		$request->getSession()->getFlashBag()->add('alert', 'Ne vous supprimez pas !');
+	    	} else {
+	    		$em->remove($userdelete);
+				$em->flush();
+				$request->getSession()->getFlashBag()->add('notice', 'Utilisateur supprimé');
+	    	}
+	    }
+
+    	return $this->redirect($this->generateUrl('loc_ihm_location_admin_users'));
+    }
+
+    public function promoteUserAction($username, Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->getUser();
+    	$userManager = $this->get('fos_user.user_manager');
+    	$userPromote = $userManager->findUserByUsername($username);
+
+    	if (!is_object($userPromote) || !$userPromote instanceof UserInterface) {
+            $request->getSession()->getFlashBag()->add('alert', 'Ca c\'est bizarre...');
+        } else {
+    		$userPromote->addRole('ROLE_ADMIN');
+    		$userManager->updateUser($userPromote);
+			$request->getSession()->getFlashBag()->add('notice', 'Utilisateur promu');
+	    }
+
+    	return $this->redirect($this->generateUrl('loc_ihm_location_admin_users'));
+    }
+
+    public function demoteUserAction($username, Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->getUser();
+    	$userManager = $this->get('fos_user.user_manager');
+    	$userDemote = $userManager->findUserByUsername($username);
+
+    	if (!is_object($userDemote) || !$userDemote instanceof UserInterface) {
+            $request->getSession()->getFlashBag()->add('alert', 'Ca c\'est bizarre...');
+        } else {
+        	if($userDemote === $user) {
+	    		$request->getSession()->getFlashBag()->add('alert', 'Ne vous destituez pas !');
+	    	} else {
+    			$userDemote->removeRole('ROLE_ADMIN');
+    			$userManager->updateUser($userDemote);
+				$request->getSession()->getFlashBag()->add('notice', 'Utilisateur destitué !');
+			}
+	    }
+
+    	return $this->redirect($this->generateUrl('loc_ihm_location_admin_users'));
     }
 }
